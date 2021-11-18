ad67476fd1d01d89fbb5f5a1f093ec517e775c43
msalihyldz <muhammet_5628@hotmail.com>
2021-11-18 19:44:27 +0300

    footer created

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  stagger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('navbarList', [
+      transition('* => *', [
+        query('li',style({ transform: 'translateY(-200%)'})),
+        query('li',
+          stagger('400ms', [
+            animate('400ms', style({ transform: 'translateY(0)'}))
+        ]))
+      ])
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
 
